fd3b057348cc983b52c0b3673374ef524d3e5999
yejx <yejinxiong01@126.com>
2020-02-17 23:20:36 +0800

    使用spring-data-jpa代替@Mapper

--- src/main/java/com/hero/ms/service/tree/mapper/ProTypeMapper.java
+++ src/main/java/com/hero/ms/service/tree/mapper/ProTypeMapper.java
@@ -1,7 +1,7 @@
 package com.hero.ms.service.tree.mapper;
 
-import com.hero.ms.service.tree.entity.ProType;
-import org.apache.ibatis.annotations.Mapper;
+import com.hero.ms.service.tree.entity.TblProCode;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
@@ -9,8 +9,7 @@ import java.util.List;
  * @Author yejx
  * @Date 2019/6/8 22:00
  */
-@Mapper
-public interface ProTypeMapper {
+public interface ProTypeMapper extends JpaRepository<TblProCode, String> {
     //查询所有
-    List<ProType> findAll();
+    List<TblProCode> findByOrderBySortNo();
 }
