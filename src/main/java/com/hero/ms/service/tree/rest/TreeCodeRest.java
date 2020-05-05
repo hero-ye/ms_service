120c64f3b3b8bc5cb013a8a51d362a56320764e4
叶金雄-合力-客服应用 <yejx@hollycrm.com>
2020-05-05 17:07:21 +0800

    更新

--- src/main/java/com/hero/ms/service/tree/rest/TreeCodeRest.java
+++ src/main/java/com/hero/ms/service/tree/rest/TreeCodeRest.java
@@ -1,9 +1,14 @@
 package com.hero.ms.service.tree.rest;
 
-import com.hero.ms.common.pojo.ServiceResult;
+import com.hero.ms.common.pojo.MsgCode;
+import com.hero.ms.common.pojo.ResponseResult;
 import com.hero.ms.common.pojo.StatusCode;
 import com.hero.ms.service.tree.entity.TblTreeCode;
 import com.hero.ms.service.tree.service.TreeCodeService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.java.Log;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,8 +30,16 @@ import java.util.Map;
 @RestController
 @RequestMapping("/tree")
 //@CrossOrigin  //解决跨域（方案一）
+@Api(value = "tree", description = "树形字典接口")
+@ApiResponses(value = {
+        @ApiResponse(code = 200, message = "请求已完成"),
+        @ApiResponse(code = 201, message = "资源成功被创建"),
+        @ApiResponse(code = 400, message = "请求中有语法问题，或不能满足请求"),
+        @ApiResponse(code = 401, message = "未授权客户机访问数据"),
+        @ApiResponse(code = 403, message = "服务器接受请求，但是拒绝处理"),
+        @ApiResponse(code = 404, message = "服务器找不到给定的资源；文档不存在"),
+        @ApiResponse(code = 500, message = "服务器出现异常")})
 public class TreeCodeRest {
-
 
     @Resource(name = "TreeCodeServiceImpl")
     private TreeCodeService treeCodeService;
@@ -36,15 +49,16 @@ public class TreeCodeRest {
      *
      * @return
      */
+    @ApiOperation(value = "条件查询树形字典", notes = "条件查询树形字典", response = ResponseResult.class)
     @RequestMapping(value = "/getTreeCodeInfo", method = RequestMethod.GET)
-    public ServiceResult getTreeCodeInfo(@RequestParam(required = false) String proId, @RequestParam String codeType) {
-        ServiceResult sr = new ServiceResult();
+    public ResponseResult getTreeCodeInfo(@RequestParam(required = false) String proId, @RequestParam String codeType) {
+        ResponseResult rr = new ResponseResult();
 
         if (StringUtils.isEmpty(codeType)) {
-            log.info(StatusCode.CODE_1000.getDesc());
-            sr.setSTATUS(StatusCode.CODE_1000.getKey());
-            sr.setMSG(StatusCode.CODE_1000.getDesc());
-            return sr;
+            log.severe(StatusCode.CODE_1000.getDesc());
+            rr.setSTATUS(StatusCode.CODE_1000.getKey());
+            rr.setMSG(StatusCode.CODE_1000.getDesc());
+            return rr;
         }
         try {
             List<TblTreeCode> root = new ArrayList<>();
@@ -63,20 +77,17 @@ public class TreeCodeRest {
                     }*/
                 }
             }
-            sr.setSUCCESS(true);
-            sr.setMSG(StatusCode.DEFAULT.getDesc());
-            sr.setDATA(root);
+            rr.setDATA(root);
         } catch (Exception e) {
             log.severe(e.getMessage());
-            sr.setSTATUS(StatusCode.CODE_3000.getKey());
-            sr.setMSG(StatusCode.CODE_3000.getDesc());
+            rr = new ResponseResult(MsgCode.FAILURE.getKey(), StatusCode.CODE_3000.getKey(), StatusCode.CODE_3000.getDesc());
         }
-        return sr;
+        return rr;
     }
 
     /*@RequestMapping("/findTreeByCodeType")
-    public ServiceResult findTree(@RequestParam Map<String, String> param) {
-        ServiceResult sr = new ServiceResult();
+    public ResponseResult findTree(@RequestParam Map<String, String> param) {
+        ResponseResult rr = new ResponseResult();
         try {
             List<TblTreeCode> root = new ArrayList<>();
             List<TblTreeCode> tblTreeCodeList = treeCodeService.findRoot(param.get("proId"), param.get("codeType"));
@@ -90,32 +101,33 @@ public class TreeCodeRest {
                     child.setChildren(children2);
                 }
             }
-            sr.setSUCCESS(true);
-            sr.setMSG("加载许可数据成功");
-            sr.setDATA(root);
+            rr.setSUCCESS(true);
+            rr.setMSG("加载许可数据成功");
+            rr.setDATA(root);
         } catch (Exception e) {
             log.severe(e.getMessage());
-            sr.setSUCCESS(false);
-            sr.setSTATUS(StatusCode.CODE_1000.getKey());
-            sr.setMSG("加载许可数据失败");
+            rr.setSUCCESS(false);
+            rr.setSTATUS(StatusCode.CODE_1000.getKey());
+            rr.setMSG("加载许可数据失败");
         }
-        return sr;
+        return rr;
     }*/
 
+    @ApiOperation(value = "条件模糊查询", notes = "条件模糊查询", response = ResponseResult.class)
     @RequestMapping(value = "/findByWord", method = RequestMethod.GET)
-    public ServiceResult findByWord(@RequestParam Map<String, String> param) {
-        ServiceResult sr = new ServiceResult();
+    public ResponseResult findByWord(@RequestParam Map<String, String> param) {
+        ResponseResult rr = new ResponseResult();
         try {
             List<TblTreeCode> tblTreeCodeList = treeCodeService.findByWord(param.get("codeName"));
-            sr.setSUCCESS(true);
-            sr.setMSG("加载许可数据成功");
-            sr.setDATA(tblTreeCodeList);
+            rr.setSUCCESS(MsgCode.DEFAULT.getKey());
+            rr.setMSG("加载许可数据成功");
+            rr.setDATA(tblTreeCodeList);
         } catch (Exception e) {
             log.severe("异步搜索：" + e.getMessage());
-            sr.setSUCCESS(false);
-            sr.setMSG("加载许可数据失败");
+            rr.setSUCCESS(MsgCode.FAILURE.getKey());
+            rr.setMSG("加载许可数据失败");
         }
-        return sr;
+        return rr;
     }
 
 }
